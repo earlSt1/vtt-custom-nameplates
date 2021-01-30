2744e5c628e676a55d27634eb7573f64746cb946
Craig <craig@earlonics.com>
2021-01-30 12:40:37 +0000

    - Local scene configuration added

--- custom-nameplates.js
+++ custom-nameplates.js
@@ -1,11 +1,23 @@
 export const modName = "Custom Nameplates";
 export const mod = "custom-nameplates";
-
-async function setGlobalConfig(){
-    let globalStyle = game.settings.get(mod,'global-style');
-    setGlobalConfigParam(globalStyle);
+export const DEFAULT_STYLE = {
+    'fontFamily':'Signika',
+    'fontSize':24,
+    'fontColor':'#FFFFFF',
+    'shadowColor':'#000000',
+    'strokeColor':'#111111'
 }
-async function setGlobalConfigParam(style){
+
+async function setSceneConfig(){
+    let globalStyle = game.settings.get(mod,'global-style');
+    let localStyles = game.settings.get(mod,'local-styles');
+    if (localStyles[game.scenes.viewed.id] != null){
+        setSceneConfigParam(localStyles[game.scenes.viewed.id]);
+    }else{
+        setSceneConfigParam(globalStyle);
+    }
+}
+async function setSceneConfigParam(style){
     CONFIG.canvasTextStyle.fontSize = style.fontSize;
     CONFIG.canvasTextStyle.fontFamily = style.fontFamily;
     CONFIG.canvasTextStyle.fill = style.fontColor;
@@ -25,23 +37,50 @@ class NameplateEditConfig extends FormApplication {
         return "Edit Nameplate Style"
     }
     async getData(options) {
+        let localSetting = game.settings.get(mod,'local-styles')[game.scenes.viewed.id]
+        let hasLocalSettings = (localSetting != null)
+        if (!localSetting){
+            localSetting = DEFAULT_STYLE;
+        }
         return {
           globalSettings: game.settings.get(mod,'global-style'),
+          localSettings: localSetting,
+          hasLocalSettings: hasLocalSettings,
           fontFamilies: CONFIG.fontFamilies
         }
       }
     async _updateObject(event, formData) {
-        let globalSettings = formData;
-        await game.settings.set(mod,'global-style',globalSettings) 
-        console.log(globalSettings)
-        ui.notifications.notify('Updated global nameplate style. Please refresh to update existing measured templates');
-        setGlobalConfigParam(globalSettings)
+        console.log(formData)
+        if (formData.localConfig){
+            let localSettings = {
+                'fontFamily':formData.localFontFamily,
+                'fontSize':formData.localFontSize,
+                'fontColor':formData.localFontColor,
+                'shadowColor':formData.localShadowColor,
+                'strokeColor':formData.localStrokeColor
+            }
+            let existingLocalStyles = game.settings.get(mod,'local-styles');
+            existingLocalStyles[game.scenes.viewed.id] = localSettings;
+            await game.settings.set(mod,'local-styles',existingLocalStyles);
+        }else{
+            let globalSettings = {
+                'fontFamily':formData.globalFontFamily,
+                'fontSize':formData.globalFontSize,
+                'fontColor':formData.globalFontColor,
+                'shadowColor':formData.globalShadowColor,
+                'strokeColor':formData.globalStrokeColor
+            }
+            await game.settings.set(mod,'global-style',globalSettings);
 
-    }
-    /** @override */
-    async close(options){
-        setGlobalConfig();
-        return super.close(options);
+            //Remove local settings (as local settings not enabled)
+            let existingLocalStyles = game.settings.get(mod,'local-styles');
+            if (existingLocalStyles[game.scenes.viewed.id] != null){
+                delete existingLocalStyles[game.scenes.viewed.id];
+                await game.settings.set(mod,'local-styles',existingLocalStyles);
+            }
+        }
+        ui.notifications.notify('Updated nameplate styles. Please refresh or change scenes to update existing measured templates');
+        setSceneConfig();
     }
 }
 
@@ -50,13 +89,17 @@ async function registerSettings(){
         scope: 'world',
         config: false,
         type: Object,
-        default:{
-            'fontFamily':'Signika',
-            'fontSize':24,
-            'fontColor':'#FFFFFF',
-            'shadowColor':'#000000',
-            'strokeColor':'#111111'
-        }
+        default:DEFAULT_STYLE
+    });
+    /*
+    * Scene specific config
+    * use game.scenes.viewed.id as key to style
+    */
+    game.settings.register(mod, 'local-styles', {
+        scope: 'world',
+        config: false,
+        type: Object,
+        default:{}
     });
     game.settings.registerMenu(mod,'settingsMenu',{
         name: 'Configuration',
@@ -67,19 +110,16 @@ async function registerSettings(){
     });
     let existing = game.settings.get(mod,'global-style')
     if (Object.keys(existing).length<5){
-        existing = {
-            'fontFamily':'Signika',
-            'fontSize':24,
-            'fontColor':'#FFFFFF',
-            'shadowColor':'#000000',
-            'strokeColor':'#111111'
-        }
+        existing = DEFAULT_STYLE
         await game.settings.set(mod,'global-style',existing)
     }
-    setGlobalConfigParam(existing);
+    setSceneConfigParam(existing);
     //Override token getTextStyle to prevent it from changing
     Token.prototype._getTextStyle = function(){return CONFIG.canvasTextStyle}
 }
 Hooks.on('setup',() => {
     registerSettings()
+    Hooks.on('canvasInit',() => {
+        setSceneConfig();
+    })
 });
